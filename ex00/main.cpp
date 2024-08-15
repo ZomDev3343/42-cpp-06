d7c46b25b419c55c066bc100b73045e198a3a3b4
zom <contact.zomzd@gmail.com>
2024-08-15 13:55:49 +0200

    gohrhgie

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,5 +5,7 @@ int main(int ac, char **av)
 	//ScalarConverter t;
 	if (ac == 2)
 		ScalarConverter::convert(av[1]);
+	else
+		std::cout << "Usage: ./scalar <number_to_convert>" << std::endl;
 	return 0;
 }
